e227025f2b3614cc65b1c61aab1db33727dd3b87
Debbie Yuen <deborahyuen2016@berkeley.edu>
2024-04-04 18:25:15 -0700

    movement and collision script

--- HelloLens/Assets/Scripts/CollisionHandler.cs
+++ HelloLens/Assets/Scripts/CollisionHandler.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] private MeshRenderer meshRenderer;
+    [SerializeField] private ParticleSystem enemyParticles;
+
+    [SerializeField] Material[] myMaterials;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+        switch (collision.gameObject.tag)
+        {
+            case "Enemy":
+                Destroy(gameObject);
+                break;
+
+            case "Gem":
+                meshRenderer.material = collision.gameObject.GetComponent<Renderer>().material;
+                Destroy(collision.gameObject);
+                PlayParticles();
+                break;
+
+            default:
+                break;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        meshRenderer.material = myMaterials[Random.Range(0, myMaterials.Length)];
+
+        StartCoroutine(loopDelay());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator loopDelay()
     {
-        
+        for (int i = 0; i < myMaterials.Length; i++)
+        {
+            meshRenderer.material = myMaterials[i];
+            yield return new WaitForSeconds(2.00f);
+        }
+        StartCoroutine(loopDelay());
+    }
+
+private void PlayParticles()
+    {
+        enemyParticles.Play();
     }
 }
--- HelloLens/Assets/Scripts/PlayerController.cs
+++ HelloLens/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
